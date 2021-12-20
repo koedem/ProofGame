6af7856f3922c72a8cdc6f33075ae0c7857db2b9
koedem <koedem64@gmail.com>
2021-12-20 04:14:53 +0100

    Reorganize TT into cointaining buckets of 8 entries each.

--- perftTT8.h
+++ perftTT8.h
@@ -4,13 +4,17 @@
 #include <iostream>
 #include <iomanip>
 
+struct __attribute__((packed, aligned(64))) Bucket {
+    uint64_t slots[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+};
+
 template<size_t SIZE_IN_MB>
 class PerftTT_8 {
 
     uint64_t  size;
     uint64_t mask;
     const uint64_t    FREQUENCY_CAP = 8;
-    alignas(64) std::vector<uint64_t> entries;
+    alignas(64) std::vector<Bucket> entries;
     alignas(64) std::vector<uint64_t> limitHitsPerDepth;
 
     uint64_t entryCount = 0, dupeCount = 0, collisionCount = 0;
@@ -19,34 +23,34 @@ public:
 
     void putEmpty(uint64_t hash) {
         uint64_t bucket = hash & mask;
-        if (entries[8 * bucket] == (hash & (~mask)) + 1 || entries[8 * bucket + 1] == (hash & (~mask)) + 1
-            || entries[8 * bucket + 2] == (hash & (~mask)) + 1 || entries[8 * bucket + 3] == (hash & (~mask)) + 1
-            || entries[8 * bucket + 4] == (hash & (~mask)) + 1 || entries[8 * bucket + 5] == (hash & (~mask)) + 1
-            || entries[8 * bucket + 6] == (hash & (~mask)) + 1 || entries[8 * bucket + 7] == (hash & (~mask)) + 1) {
+        if (entries[bucket].slots[0] == (hash & (~mask)) + 1 || entries[bucket].slots[1] == (hash & (~mask)) + 1
+            || entries[bucket].slots[2] == (hash & (~mask)) + 1 || entries[bucket].slots[3] == (hash & (~mask)) + 1
+            || entries[bucket].slots[4] == (hash & (~mask)) + 1 || entries[bucket].slots[5] == (hash & (~mask)) + 1
+            || entries[bucket].slots[6] == (hash & (~mask)) + 1 || entries[bucket].slots[7] == (hash & (~mask)) + 1) {
             dupeCount++;
-        } else if (entries[8 * bucket] == 0) {
-            entries[8 * bucket] = (hash & (~mask)) + 1;
+        } else if (entries[bucket].slots[0] == 0) {
+            entries[bucket].slots[0] = (hash & (~mask)) + 1;
             entryCount++;
-        } else if (entries[8 * bucket + 1] == 0) {
-            entries[8 * bucket + 1] = (hash & (~mask)) + 1;
+        } else if (entries[bucket].slots[1] == 0) {
+            entries[bucket].slots[1] = (hash & (~mask)) + 1;
             entryCount++;
-        } else if (entries[8 * bucket + 2] == 0) {
-            entries[8 * bucket + 2] = (hash & (~mask)) + 1;
+        } else if (entries[bucket].slots[2] == 0) {
+            entries[bucket].slots[2] = (hash & (~mask)) + 1;
             entryCount++;
-        } else if (entries[8 * bucket + 3] == 0) {
-            entries[8 * bucket + 3] = (hash & (~mask)) + 1;
+        } else if (entries[bucket].slots[3] == 0) {
+            entries[bucket].slots[3] = (hash & (~mask)) + 1;
             entryCount++;
-        } else if (entries[8 * bucket + 4] == 0) {
-            entries[8 * bucket + 4] = (hash & (~mask)) + 1;
+        } else if (entries[bucket].slots[4] == 0) {
+            entries[bucket].slots[4] = (hash & (~mask)) + 1;
             entryCount++;
-        } else if (entries[8 * bucket + 5] == 0) {
-            entries[8 * bucket + 5] = (hash & (~mask)) + 1;
+        } else if (entries[bucket].slots[5] == 0) {
+            entries[bucket].slots[5] = (hash & (~mask)) + 1;
             entryCount++;
-        } else if (entries[8 * bucket + 6] == 0) {
-            entries[8 * bucket + 6] = (hash & (~mask)) + 1;
+        } else if (entries[bucket].slots[6] == 0) {
+            entries[bucket].slots[6] = (hash & (~mask)) + 1;
             entryCount++;
-        } else if (entries[8 * bucket + 7] == 0) {
-            entries[8 * bucket + 7] = (hash & (~mask)) + 1;
+        } else if (entries[bucket].slots[7] == 0) {
+            entries[bucket].slots[7] = (hash & (~mask)) + 1;
             entryCount++;
         } else {
             collisionCount++;
@@ -56,10 +60,10 @@ public:
     void incrementPosition(uint64_t hash, bool ruined) {
         uint64_t bucket = hash & mask;
         for (uint64_t i = 0; i < 8; i++) {
-            if ((entries[8 * bucket + i] & (~mask)) == (hash & (~mask))) {
+            if ((entries[bucket].slots[i] & (~mask)) == (hash & (~mask))) {
                 uint64_t count;
-                if ((count = entries[8 * bucket + i] & mask) < mask) { // once we reach the mask, i.e. equals, we stop incrementing
-                    entries[8 * bucket + i] = (entries[8 * bucket + i] & (~mask)) + count + (ruined ? 2 : 1);
+                if ((count = entries[bucket].slots[i] & mask) < mask) { // once we reach the mask, i.e. equals, we stop incrementing
+                    entries[bucket].slots[i] = (entries[bucket].slots[i] & (~mask)) + count + (ruined ? 2 : 1);
                 }
                 break;
             }
@@ -69,8 +73,8 @@ public:
     bool isUnique(uint64_t hash) {
         uint64_t bucket = hash & mask;
         for (uint64_t i = 0; i < 8; i++) {
-            if ((entries[8 * bucket + i] & (~mask)) == (hash & (~mask))) {
-                if ((entries[8 * bucket + i] & mask) <= 7 && (entries[8 * bucket + i] & mask) == 2) { // two means once created then once reached from higher depth
+            if ((entries[bucket].slots[i] & (~mask)) == (hash & (~mask))) {
+                if ((entries[bucket].slots[i] & mask) <= 7 && (entries[bucket].slots[i] & mask) == 2) { // two means once created then once reached from higher depth
                     return true;
                 }
                 break;
@@ -82,10 +86,10 @@ public:
     uint64_t incrementToLimit(uint64_t hash, int limit, int depth) {
         uint64_t bucket = hash & mask;
         for (uint64_t i = 0; i < 8; i++) {
-            if ((entries[8 * bucket + i] & (~mask)) == (hash & (~mask))) {
+            if ((entries[bucket].slots[i] & (~mask)) == (hash & (~mask))) {
                 uint64_t count;
-                if ((count = entries[8 * bucket + i] & mask) < limit) { // once we reach the limit, i.e. equals, we stop incrementing
-                    entries[8 * bucket + i] = (entries[8 * bucket + i] & (~mask)) + count + 1;
+                if ((count = entries[bucket].slots[i] & mask) < limit) { // once we reach the limit, i.e. equals, we stop incrementing
+                    entries[bucket].slots[i] = (entries[bucket].slots[i] & (~mask)) + count + 1;
                 } else {
                     limitHitsPerDepth[depth]++;
                 }
@@ -94,29 +98,29 @@ public:
             }
         }
 
-        if (entries[8 * bucket] == 0) {
-            entries[8 * bucket] = (hash & (~mask)) + 1;
+        if (entries[bucket].slots[0] == 0) {
+            entries[bucket].slots[0] = (hash & (~mask)) + 1;
             entryCount++;
-        } else if (entries[8 * bucket + 1] == 0) {
-            entries[8 * bucket + 1] = (hash & (~mask)) + 1;
+        } else if (entries[bucket].slots[1] == 0) {
+            entries[bucket].slots[1] = (hash & (~mask)) + 1;
             entryCount++;
-        } else if (entries[8 * bucket + 2] == 0) {
-            entries[8 * bucket + 2] = (hash & (~mask)) + 1;
+        } else if (entries[bucket].slots[2] == 0) {
+            entries[bucket].slots[2] = (hash & (~mask)) + 1;
             entryCount++;
-        } else if (entries[8 * bucket + 3] == 0) {
-            entries[8 * bucket + 3] = (hash & (~mask)) + 1;
+        } else if (entries[bucket].slots[3] == 0) {
+            entries[bucket].slots[3] = (hash & (~mask)) + 1;
             entryCount++;
-        } else if (entries[8 * bucket + 4] == 0) {
-            entries[8 * bucket + 4] = (hash & (~mask)) + 1;
+        } else if (entries[bucket].slots[4] == 0) {
+            entries[bucket].slots[4] = (hash & (~mask)) + 1;
             entryCount++;
-        } else if (entries[8 * bucket + 5] == 0) {
-            entries[8 * bucket + 5] = (hash & (~mask)) + 1;
+        } else if (entries[bucket].slots[5] == 0) {
+            entries[bucket].slots[5] = (hash & (~mask)) + 1;
             entryCount++;
-        } else if (entries[8 * bucket + 6] == 0) {
-            entries[8 * bucket + 6] = (hash & (~mask)) + 1;
+        } else if (entries[bucket].slots[6] == 0) {
+            entries[bucket].slots[6] = (hash & (~mask)) + 1;
             entryCount++;
-        } else if (entries[8 * bucket + 7] == 0) {
-            entries[8 * bucket + 7] = (hash & (~mask)) + 1;
+        } else if (entries[bucket].slots[7] == 0) {
+            entries[bucket].slots[7] = (hash & (~mask)) + 1;
             entryCount++;
         } else {
             collisionCount++;
@@ -140,12 +144,15 @@ public:
 
     void printFrequencies() {
         std::vector<uint64_t> frequencies(FREQUENCY_CAP + 1);
-        for (uint64_t entry : entries) {
-            size_t frequency = entry & mask;
-            if (frequency < FREQUENCY_CAP) {
-                frequencies[frequency]++;
-            } else {
-                frequencies[FREQUENCY_CAP]++;
+        for (Bucket bucket : entries) {
+            for (int i = 0; i < 8; i++) {
+                uint64_t entry = bucket.slots[i];
+                size_t frequency = entry & mask;
+                if (frequency < FREQUENCY_CAP) {
+                    frequencies[frequency]++;
+                } else {
+                    frequencies[FREQUENCY_CAP]++;
+                }
             }
         }
         for (size_t i = 0; i < frequencies.size(); i++) {
@@ -156,7 +163,12 @@ public:
     }
 
     void reset() {
-        std::fill(entries.begin(), entries.end(), 0);
+        std::fill(entries.begin(), entries.end(), Bucket());
+        for (Bucket& bucket : entries) {
+            for (uint64_t & slot : bucket.slots) {
+                slot = 0;
+            }
+        }
         std::fill(limitHitsPerDepth.begin(), limitHitsPerDepth.end(), 0);
         entryCount = 0;
         dupeCount = 0;
@@ -166,6 +178,6 @@ public:
     explicit PerftTT_8() : limitHitsPerDepth(10),
             size(SIZE_IN_MB * 131072),
             mask(size / 8 - 1), // eight entries per bucket
-            entries(size) {
+            entries(size / 8) {
     }
 };
